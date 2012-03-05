6354684a2fb0b17ab6302923a569af30bd358cee
akre54 <krebsadam@gmail.com>
2012-03-05 00:39:05 -0500

    more fun with api keys

--- includes/utils.php
+++ includes/utils.php
@@ -1,8 +1,20 @@
 <?php
 
+require_once('db.php');
+
 if (isset($_REQUEST['SESSION']) ){
 	print "nope.";
 	exit(999);
+}
+
+if (!$isset($_GET['api_key']) || !check_api_key($_GET['api_key'])) {
+	failure('invalid API key or API key not set'):
+}
+
+function check_api_key($apiKey) {
+	$db = new mysql();
+	
+	return $db->get('api_clients', 'client_name', "api_key = '$apiKey'");
 }
 
 function make_password($pass, $salt=generateSalt()) {
@@ -20,6 +32,15 @@ function generateSalt($max = 15) {
 		$i++;
 	} while ($i < $max);
 	return $salt;
+}
+
+function failure($message) {
+	$response = array();
+	$response['status'] = 'failure';
+	$response['reason'] = $message;
+	print json_encode($response);
+	log('failure: ' . $message . implode(', ', $_POST));
+	exit(9);
 }
 
 
