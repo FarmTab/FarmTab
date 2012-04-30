305e15b298da76aa061dacdd6bae7436ab361f54
Adam Krebs <amk528@cs.nyu.edu>
2012-04-29 22:50:33 -0700

    first try at using require.js in specrunner per http://japhr.blogspot.com/2011/12/jasmine-testing-of-requirejs-powered.html. nothin doing yet

--- demo/app/collections/Farm.js
+++ demo/app/collections/Farm.js
@@ -1,16 +1,15 @@
 define( ['jquery', 'backbone', 'models/Customer'],
-        function( $, Backbone, Customer ) {
-            // Using ECMAScript 5 strict mode during development. By default r.js will ignore that.
-            "use strict";
+  function( $, Backbone, Customer ) {
+    // Using ECMAScript 5 strict mode during development. By default r.js will ignore that.
+    "use strict";
+    
+    var Farm = Backbone.Collection.extend( {
+      model: Customer
+    } );
+    
+    listCustomers: function() {
+      return this.filter(function(customer){ return customer.get('farmId'); });
+    }
 
-
-            var Farm = Backbone.Collection.extend( {
-                model: Customer
-            } );
-            
-            listCustomers: function() {
-            	return this.filter(function(customer){ return customer.get('farmId'); });
-            }
-
-            return Farm;
-        } );
+    return Farm;
+} );
